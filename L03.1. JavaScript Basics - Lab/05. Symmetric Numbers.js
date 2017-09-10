658b64a7e6d5cfa2cfa0316fc8401282c05d0b3b
Viktor Aleksandrov <vikinga07@abv.bg>
2017-09-11 00:05:09 +0300

    Add folder L03.1. JavaScript Basics - Lab

---
+++ L03.1. JavaScript Basics - Lab/05. Symmetric Numbers.js
@@ -0,0 +1,24 @@
+function symmetricNumbers(arr) {
+    let num = Number(arr[0]);
+    let result = '';
+
+    for (let index = 1; index <= num; index++) {
+        if (isSymmetric(index.toString())) {
+            result += index + ' ';
+        }
+    }
+
+    console.log(result);
+
+    function isSymmetric(strNum) {
+        for (let pos = 0; pos < strNum.length / 2; pos++) {
+            if (strNum[pos] != strNum[strNum.length - pos - 1]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
+
+symmetricNumbers(['1000']);
