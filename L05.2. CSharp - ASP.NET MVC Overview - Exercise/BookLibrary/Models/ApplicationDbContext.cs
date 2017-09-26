d2142bc92e041dece063c0791c64f4f613da6c93
Viktor Aleksandrov <vikinga07@abv.bg>
2017-09-26 18:26:39 +0300

    Add folder L05.2. CSharp - ASP.NET MVC Overview - Exercise

---
+++ L05.2. CSharp - ASP.NET MVC Overview - Exercise/BookLibrary/Models/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace BookLibrary.Models
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public virtual DbSet<Book> Books { get; set; }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
