bc831ac9ab8ac9fc631bf21e33500a7ef199bbbf
Viktor Aleksandrov <vikinga07@abv.bg>
2017-11-16 01:20:31 +0200

    Modify file p07. Largest 3 Numbers.js in folder L02.1. JavaScript Basics - Lab

--- L02.1. JavaScript Basics - Lab/p07. Largest 3 Numbers.js
+++ L02.1. JavaScript Basics - Lab/p07. Largest 3 Numbers.js
@@ -1,7 +1,7 @@
 function printLargestThreeNumbers(arr) {
 
     console.log(arr.map(Number)
-        .sort((a, b) => b > a)
+        .sort((a, b) => b - a)
         .slice(0, 3)
         .join('\r\n'));
 }
